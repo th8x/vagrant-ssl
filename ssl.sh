b8e5eabd64d89263f2463133a4733aeb237733c8
Thanh Huynh <trthanhhuynh@gmail.com>
2018-06-01 17:05:24 +0700

    Create ssl.sh

---
+++ ssl.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: Must supply a domain"
+  exit 1
+fi
+
+DOMAIN=$1
+
+cd ~/certs
+
+openssl genrsa -out $DOMAIN.key 2048
+openssl req -new -key $DOMAIN.key -out $DOMAIN.csr
+
+cat > $DOMAIN.ext << EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = $DOMAIN
+DNS.2 = $DOMAIN.192.168.1.19.xip.io
+EOF
+
+openssl x509 -req -in $DOMAIN.csr -CA ../myCA.pem -CAkey ../myCA.key -CAcreateserial \
+-out $DOMAIN.crt -days 1825 -sha256 -extfile $DOMAIN.ext
